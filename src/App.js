05539879c76e8f819c10bfc4bfdb8f1f9d2fa5fa
= <Ehabr518@gmail.com>
2021-07-27 08:10:53 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
 import HomePage from "./pages/homepage/homepage.component";
+
+
 function App() {
   return (
     <div>
-     <HomePage/>
+        <Route exact path='/' component={HomePage} />
     </div>
   );
 }
